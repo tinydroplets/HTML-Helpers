85143ad68b1f47d15ece56f9ee215323a2a60a0a
tinydroplets <amamuwala@WBLT32KPWZ1>
2016-12-13 07:59:34 -0600

    added a static label helper

---
+++ Helpers-Basic/Helpers-Basic/Helpers/LabelHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace Helpers_Basic.Helpers
+{
+    public class LabelHelper
+    {
+        public static IHtmlString Label(string target, string text)
+        {
+            return new HtmlString($"<label for='{target}'>{text}</label>");
+        }
+    }
+}
