e34228cc0c2ac738ed8a8e538b014dd2cbb097b8
tinydroplets <amamuwala@WBLT32KPWZ1>
2016-12-13 08:32:21 -0600

    html extension method

---
+++ Helpers-Basic/Helpers-Basic/Helpers/HtmlExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Helpers_Basic.Helpers
+{
+    public static  class HtmlExtensionMethods
+    {
+        public static IHtmlString LabelWithMark(this HtmlHelper helper, string content)
+        {
+            var htmlString = $"<label><mark>{content}<mark></label>";
+            return new HtmlString(htmlString);
+        }
+    }
+}
